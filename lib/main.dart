0eb134150765c4dff503fc0f6376673e4e0dfc65
firdharc <firdharc@gmail.com>
2022-02-17 15:05:21 +0700

    pid

--- lib/main.dart
+++ lib/main.dart
@@ -84,76 +84,66 @@ int nilai = 0;
 
 void main() {
   runApp(const MaterialApp(
-    title: 'MetClean',
-    home: HalamanSatu(),
-  ));
+      title: 'MetClean',
+      home: HalamanSatu(),
+      debugShowCheckedModeBanner: false));
 }
 
+//-------------------------Page Awal (pidra)------------------------------------
 class HalamanSatu extends StatelessWidget {
   const HalamanSatu({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("MetClean"),
-        ),
         body: Column(children: [
-          new Image.network(
-              'https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/2x/external-cleaning-labour-day-vitaliy-gorbachev-flat-vitaly-gorbachev.png',
-              width: 250,
-              height: 250,
-              fit: BoxFit.cover),
-          SizedBox(
-            height: 50,
-          ),
-          Container(
-              padding: EdgeInsets.symmetric(horizontal: 40),
-              child: Center(
-                child: Text(
-                  'Cara mudah dan \nandal untuk merawat rumah Anda\n',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey.shade900,
-                  ),
-                ),
-              )),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 60),
-            child: Center(
-              child: Text(
-                'Kami menyediakan Anda dengan orang-orang terbaik untuk membantu mengurus rumah Anda.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey.shade600,
-                ),
+      new Image.network(
+          'https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/2x/external-cleaning-labour-day-vitaliy-gorbachev-flat-vitaly-gorbachev.png',
+          width: 250,
+          height: 250,
+          fit: BoxFit.cover),
+      SizedBox(
+        height: 50,
+      ),
+      Container(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Center(
+            child: Text(
+              'Clean Home, Clean Life\n',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey.shade900,
               ),
             ),
+          )),
+      Container(
+        padding: EdgeInsets.symmetric(horizontal: 60),
+        child: Center(
+          child: Text(
+            'Suasana tempat tinggal yang kotor bikin kamu jadi gak nyaman?\n Kru berpengalaman, langsung dari kami, memastikan anda mendapatkan pengalaman terbaik.\n Sewa sekarang juga !!',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.grey.shade600,
+            ),
           ),
-          Padding(
-              padding: EdgeInsets.all(50.0),
-              child: MaterialButton(
-                elevation: 0,
-                color: Colors.black,
-                onPressed: () {},
-                height: 55,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                child: Center(
-                  child: Text(
-                    'Mulai',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-              ))
-        ]));
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.all(20),
+        height: 35,
+        width: 1300,
+        color: Colors.black,
+        child: ElevatedButton(
+            child: const Text('Mulai'),
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => HalamanDua(kirim: nilai.toString())));
+            }),
+      ),
+    ]));
   }
 }
 
