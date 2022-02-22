93c6266e9dfb0e878842fcf3823cddf115377e08
avrlyn <avriliyyani@gmail.com>
2022-02-22 19:13:42 +0700

    nampilin kalender n benerin button di page 3

--- lib/pages/halaman_ketiga.dart
+++ lib/pages/halaman_ketiga.dart
@@ -3,221 +3,109 @@ import '/main.dart';
 import '/pages/halaman_empat.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-// class HalamanTiga extends StatefulWidget {
-//   HalamanTiga({Key? key, required this.kirim}) : super(key: key);
-//   String kirim;
+import 'package:flutter/material.dart';
 
-//   @override
-//   State<HalamanTiga> createState() => _HalamanTigaState();
-// }
-
-// class _HalamanTigaState extends State<HalamanTiga> {
-//   CalendarFormat format = CalendarFormat.month;
-//   DateTime selectedDay = DateTime.now();
-//   DateTime focusedDay = DateTime.now();
-
-// @override
-// Widget build(BuildContext context) {
-//   return Scaffold(
-//     appBar: AppBar(
-//       title: const Text("MetClean"),
-//       backgroundColor: Colors.purple[300],
-//       actions: <Widget>[
-//         IconButton(icon: Icon(Icons.account_circle), onPressed: () {}),
-//       ],
-//     ),
-// body: Center(
-//   child: TableCalendar(
-//     firstDay: DateTime(2010),
-//     lastDay: DateTime(2040),
-//     focusedDay: selectedDay,
-//     calendarFormat: format,
-//     onFormatChanged: (CalendarFormat _format) {
-//       setState(() {
-//         format = _format;
-//       });
-//     },
-//     startingDayOfWeek: StartingDayOfWeek.sunday,
-//     daysOfWeekVisible: true,
-
-//     //kodingan perubahan hari
-//     onDaySelected: (DateTime selectDay, DateTime focusDay) {
-//       setState(() {
-//         selectedDay = selectDay;
-//         focusedDay = focusDay;
-//       });
-//       print(focusedDay);
-//     },
-//     selectedDayPredicate: (DateTime date) {
-//       return isSameDay(selectedDay, date);
-//     },
-
-//     //kodingan kostum kalender
-//     calendarStyle: CalendarStyle(
-//       isTodayHighlighted: true,
-//       selectedDecoration: BoxDecoration(
-//         color: Colors.deepPurple,
-//         shape: BoxShape.circle,
-//       ),
-//       selectedTextStyle: TextStyle(
-//           fontSize: 20,
-//           color: Colors.white,
-//           fontWeight: FontWeight.bold),
-//       todayDecoration: BoxDecoration(
-//         color: Colors.purple.shade300,
-//         shape: BoxShape.circle,
-//       ),
-//     ),
-//     headerStyle: HeaderStyle(
-//       formatButtonVisible: false,
-//       titleCentered: true,
-//     ),
-//   ),
-//   body: Center(
-//     child: Column(
-//       children: [
-//         Container(
-//           margin: EdgeInsets.all(20),
-//           height: 35,
-//           width: 100,
-//           child: ElevatedButton(
-//               style: ElevatedButton.styleFrom(
-//                 primary: Colors.purple[300],
-//                 onPrimary: Colors.white,
-//               ),
-//               child: const Text('Pesan'),
-//               onPressed: () {
-//                 Navigator.of(context).push(MaterialPageRoute(
-//                     builder: (context) =>
-//                         HalamanTiga(kirim: nilai.toString())));
-//               }),
-//         ),
-//       ],
-//     ),
-//   ),
-// )
-
-//     child: Column(children: <Widget>[
-//   Container(
-//     child: Text("Apakah hidung anda tersumbat (y/t)?"),
-//   ),
-//   Container(
-//       child: TextField(
-//     controller: nilaisurvei3,
-//     style: TextStyle(fontSize: 24, color: Colors.black),
-//   )),
-//   Container(
-//     margin: EdgeInsets.all(20),
-//     child: ElevatedButton(
-//         child: const Text('Ke Halaman Empat'),
-//         onPressed: () {
-//           if (nilaisurvei3.text == "y") {
-//             nilai = int.parse(kirim) + 20;
-//           } else {
-//             nilai = int.parse(kirim);
-//           }
-//           Navigator.of(context).push(MaterialPageRoute(
-//               builder: (context) =>
-//                   HalamanEmpat(kirim: nilai.toString())));
-//         }),
-//   ),
-// ]
-//     );
-//   }
-// }
-
-// class HalamanTiga extends StatelessWidget {
-//   String kirim;
-//   HalamanTiga({Key? key, required this.kirim}) : super(key: key);
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: const Text("MetClean"),
-//           backgroundColor: Colors.purple[300],
-//           actions: <Widget>[
-//             IconButton(icon: Icon(Icons.account_circle), onPressed: () {}),
-//           ],
-//         ),
-//         body: Center(
-//             child: Column(children: <Widget>[
-//           Container(
-//             child: Text("Apakah anda sakit batuk atau pilek?"),
-//           ),
-//           Container(
-//               child: TextField(
-//             controller: nilaisurvei2,
-//             style: TextStyle(fontSize: 24, color: Colors.white),
-//           )),
-//           Container(
-//             margin: EdgeInsets.all(20),
-//             child: ElevatedButton(
-//                 child: const Text('Ke Halaman Empat'),
-//                 onPressed: () {
-//                   if (nilaisurvei2.text == "y") {
-//                     nilai = int.parse(kirim) + 20;
-//                   } else {
-//                     nilai = int.parse(kirim);
-//                   }
-//                   Navigator.of(context).push(MaterialPageRoute(
-//                       builder: (context) =>
-//                           HalamanEmpat(kirim: nilai.toString())));
-//                 }),
-//           ),
-//         ])));
-//   }
-// }
-
-TextEditingController total = TextEditingController();
-
-class HalamanTiga extends StatelessWidget {
-  String kirim;
+class HalamanTiga extends StatefulWidget {
   HalamanTiga({Key? key, required this.kirim}) : super(key: key);
+  String kirim;
+
+  @override
+  State<HalamanTiga> createState() => _HalamanTigaState();
+}
+
+class _HalamanTigaState extends State<HalamanTiga> {
+  CalendarFormat format = CalendarFormat.month;
+  DateTime selectedDay = DateTime.now();
+  DateTime focusedDay = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("MetClean"),
-          backgroundColor: Colors.purple[300],
-          actions: <Widget>[
-            IconButton(icon: Icon(Icons.account_circle), onPressed: () {}),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: const Text("MetClean"),
+        backgroundColor: Colors.purple[300],
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.account_circle), onPressed: () {}),
+        ],
+      ),
+      body: Container(
+        child: ListView(
+          children: [
+            TableCalendar(
+              firstDay: DateTime(2010),
+              lastDay: DateTime(2040),
+              focusedDay: selectedDay,
+              calendarFormat: format,
+              onFormatChanged: (CalendarFormat _format) {
+                setState(() {
+                  format = _format;
+                });
+              },
+              startingDayOfWeek: StartingDayOfWeek.sunday,
+              daysOfWeekVisible: true,
+
+              //kodingan perubahan hari
+              onDaySelected: (DateTime selectDay, DateTime focusDay) {
+                setState(() {
+                  selectedDay = selectDay;
+                  focusedDay = focusDay;
+                });
+                print(focusedDay);
+              },
+              selectedDayPredicate: (DateTime date) {
+                return isSameDay(selectedDay, date);
+              },
+
+              //kodingan kostum kalender
+              calendarStyle: CalendarStyle(
+                isTodayHighlighted: true,
+                selectedDecoration: BoxDecoration(
+                  color: Colors.purple[300],
+                  shape: BoxShape.circle,
+                ),
+                selectedTextStyle: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+                todayDecoration: BoxDecoration(
+                  color: Colors.purple[300],
+                  shape: BoxShape.circle,
+                ),
+              ),
+              headerStyle: HeaderStyle(
+                formatButtonVisible: false,
+                titleCentered: true,
+              ),
+            ),
+            Center(
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(20),
+                    height: 35,
+                    width: 100,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.purple[300],
+                          onPrimary: Colors.white,
+                        ),
+                        child: const Text('Pesan'),
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) =>
+                                  HalamanEmpat(kirim: nilai.toString())));
+                        }),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
-        body: Center(
-            child: Column(children: <Widget>[
-          Container(
-            child: Text("Apakah anda sakit batuk atau pilek?"),
-          ),
-          Container(
-              child: TextField(
-            controller: nilaisurvei2,
-            style: TextStyle(fontSize: 24, color: Colors.white),
-          )),
-          Container(
-            margin: EdgeInsets.all(20),
-            child: TextField(
-                controller: total,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Total',
-                ),
-                onChanged: (text) {}),
-          ),
-          Container(
-            margin: EdgeInsets.all(20),
-            child: ElevatedButton(
-                child: const Text('Ke Halaman Empat'),
-                onPressed: () {
-                  if (nilaisurvei2.text == "y") {
-                    nilai = int.parse(kirim) + 20;
-                  } else {
-                    nilai = int.parse(kirim);
-                  }
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) =>
-                          HalamanEmpat(kirim: nilai.toString())));
-                }),
-          ),
-        ])));
+      ),
+    );
   }
 }
